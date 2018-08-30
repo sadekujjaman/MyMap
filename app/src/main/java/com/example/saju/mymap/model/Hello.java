d3f2677087b20e69845efb307b354fc608e21545
Saju <sadekujjamansaju474@gmail.com>
2018-08-30 07:14:36 +0600

    Google Map Api and Google Places Api
    Version 1 Reform Again

---
+++ app/src/main/java/com/example/saju/mymap/model/Hello.java
@@ -0,0 +1,12 @@
+package com.example.saju.mymap.model;
+
+/**
+ * Created by Saju on 8/30/2018.
+ */
+
+public class Hello {
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
